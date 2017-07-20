ebb0a7ca33d534c56921b6fe1380149bd6214a23
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-07-20 15:31:56 -0500

    Show list of cards

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const token = 'YOUR TRELLO TOKEN' // https://trello.com/app-key
 
 const url = `https://api.trello.com/1/members/${user}?`
 const url2 = `https://api.trello.com/1/boards/`
+const url3 = `https://api.trello.com/1/boards/`
 
 module.exports = {
   getBoards() {
@@ -21,6 +22,16 @@ module.exports = {
   },
   getLists(boardId) {
     const completeUrl = `${url2}${boardId}/lists?cards=none&key=${key}&token=${token}`
+    return axios.get(completeUrl)
+      .then(res => {
+        return res.data
+      })
+      .catch(err => {
+        return err
+      })
+  },
+  getCards(boardId) {
+    const completeUrl = `${url3}${boardId}/cards?fields=id,name,idList,url&key=${key}&token=${token}`
     return axios.get(completeUrl)
       .then(res => {
         return res.data
--- taco.js
+++ taco.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer')
 const api = require('./api')
 
 const taco = {
@@ -19,15 +20,54 @@ const taco = {
           if (board.name === arg) {
             const promise = api.getLists(board.id)
             promise.then(res => {
+              let payload = []
               res.forEach(list => {
-                console.log(list.name)
+                let obj = { id: list.id, name: list.name }
+                payload.push(obj)
               })
+              prompt(board.id, payload)
             })
           }
         })
       })
     }
   }
+}
+
+const buildOpts = payload => {
+  let choices = []
+  payload.forEach(p => {
+    choices.push(p.name)
+  })
+  const opts = [{
+    type: 'list',
+    name: 'board',
+    message: 'What do you want to do?',
+    choices
+  }]
+  return [opts, payload]
+}
+
+const prompt = (boardId, payload) => {
+  const opts = buildOpts(payload)
+  inquirer.prompt(opts[0]).then(answer => {
+    const list = answer.board
+    const selectedItem = payload.find(el => {
+      return el.name === list
+    })
+    const promise = api.getCards(boardId)
+    promise.then(res => {
+      res.forEach(card => {
+        if (card.idList === selectedItem.id) {
+          console.log(card.name)
+        }
+      })
+    })
+  }).catch(err => {
+    console.log('')
+    console.log('Something went wrong.')
+    process.exit(0)
+  })
 }
 
 module.exports = taco

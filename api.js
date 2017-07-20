8bf0344bea3a35eaf12d49eea7f1d9a34cba31a6
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-07-20 15:35:01 -0500

    Refactor API urls

--- api.js
+++ api.js
@@ -4,14 +4,13 @@ const user = 'YOUR TRELLO USER' // ex: Taco
 const key = 'YOUR TRELLO KEY' // https://trello.com/app-key
 const token = 'YOUR TRELLO TOKEN' // https://trello.com/app-key
 
-const url = `https://api.trello.com/1/members/${user}?`
-const url2 = `https://api.trello.com/1/boards/`
-const url3 = `https://api.trello.com/1/boards/`
+const membersUrl = `https://api.trello.com/1/members`
+const boardsUrl = `https://api.trello.com/1/boards`
 
 module.exports = {
   getBoards() {
     const fields = 'url&boards=open&board_fields=name'
-    const completeUrl = `${url}fields=${fields}&key=${key}&token=${token}`
+    const completeUrl = `${membersUrl}/${user}?fields=${fields}&key=${key}&token=${token}`
     return axios.get(completeUrl)
       .then(res => {
         return res.data.boards
@@ -21,7 +20,7 @@ module.exports = {
       })
   },
   getLists(boardId) {
-    const completeUrl = `${url2}${boardId}/lists?cards=none&key=${key}&token=${token}`
+    const completeUrl = `${boardsUrl}/${boardId}/lists?cards=none&key=${key}&token=${token}`
     return axios.get(completeUrl)
       .then(res => {
         return res.data
@@ -31,7 +30,7 @@ module.exports = {
       })
   },
   getCards(boardId) {
-    const completeUrl = `${url3}${boardId}/cards?fields=id,name,idList,url&key=${key}&token=${token}`
+    const completeUrl = `${boardsUrl}/${boardId}/cards?fields=id,name,idList,url&key=${key}&token=${token}`
     return axios.get(completeUrl)
       .then(res => {
         return res.data
